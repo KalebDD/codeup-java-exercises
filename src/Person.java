fd604cabc36a22777363c36b5b8da7f2a6aebfa2
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-04-01 22:28:09 -0500

    reviewing the basics, reformatted exercise for readability

--- src/Person.java
+++ src/Person.java
@@ -2,34 +2,44 @@ public class Person {
 
     private String name;
 
-    // constructor
-    public Person(String name) {
-        this.name = name;
+    // Constructor
+    public Person(String constructorName) {
+        this.name = constructorName;
     }
 
-    // returns the person's name
+    // Getter for name
     public String getName() {
         return name;
     }
 
-    // changes the name property to the passed value
+    // Setter for name
     public void setName(String name) {
         this.name = name;
     }
 
-    // prints a message to the console using the person's name
+    // Method for saying hello
     public void sayHello() {
-        System.out.println("Hello " + name + ".");
+        System.out.println("Hello "+name+"!");
     }
 
+    public static void main (String[] args) {
 
-    public static void main(String[] args) {
-        Person kaleb = new Person("Kaleb Daniel");
-        System.out.println("kaleb.name = " + kaleb.name);
+        // Q.1
+        // Creating a new person object
+        Person kaleb = new Person("Kaleb Dworsky");
+        // Using the getName and sayHello method on the Person object stored in the variable 'kaleb'
+        System.out.println("\nThis newly created person is: " + kaleb.getName());
         kaleb.sayHello();
+        // Calling the variable 'kaleb' shows it's location in memory in hex form
+        System.out.println(kaleb);
 
-        kaleb.setName("Test");
-        System.out.println("new-name-after-setting.name = " + kaleb.name);
+
+        // Testing what happens when setting a value on a created object
+        System.out.println("\n");
+        kaleb.setName("Cameron Walker");
+        // Location in memory stays the same - doesn't create a new object
+        System.out.println(kaleb);
+        System.out.println("After setting a name on an already created object, my new name is: " + kaleb.getName());
         kaleb.sayHello();
 
         // Q.2
@@ -43,7 +53,7 @@ public class Person {
 
         // Person person1 = new Person("John");
         // Person person2 = person1;
-        // Below is true - person2 equals person1, meaning both are referencing the         same point in memory
+        // Below is true - person2 equals person1, meaning both are referencing the same point in memory
         // System.out.println(person1 == person2);
 
         // Person person1 = new Person("John");
@@ -53,7 +63,6 @@ public class Person {
         // person2.setName("Jane");
         // System.out.println(person1.getName());
         // System.out.println(person2.getName());
-        // Prints John John Jane Jane (thought it'd be 3 John's and 1 Jane) Once you        set person2's name it changes the value it's referencing, hence why person1        also becomes Jane
+        // Prints John John Jane Jane (thought it'd be 3 John's and 1 Jane) Once you set person2's name it changes the value it's referencing, hence why person1 also becomes Jane
     }
-
 }
